9bf3d62b7526819a878b5aa7b24b2c6752abfd60
Rodrigo Manhães <rmanhaes@gmail.com>
2011-09-13 23:07:16 -0300

    added ball moving, paddle collision and score counting

--- html5.games/ch02/pingpong/js/html5games.pingpong.js
+++ html5.games/ch02/pingpong/js/html5games.pingpong.js
@@ -5,7 +5,10 @@ var KEY = {
   S: 83
 };
 
-var pingpong = {};
+var pingpong = {
+  scoreA: 0,
+  scoreB: 0
+};
 pingpong.pressedKeys = [];
 
 $(function() {
@@ -21,6 +24,7 @@ $(function() {
 });
 
 function gameloop() {
+  moveBall();
   movePaddles();
 }
 
@@ -41,5 +45,98 @@ function movePaddles() {
     var top = parseInt($("#paddleA").css("top"));
     $("#paddleA").css("top", top + 5);
   }
+}
+
+
+pingpong.ball = {
+  speed: 5,
+  x: 150,
+  y: 100,
+  directionX: 1,
+  directionY: 1
+};
+
+function moveBall() {
+  var playgroundHeight = parseInt($("#playground").height());
+  var playgroundWidth = parseInt($("#playground").width());
+  var ball = pingpong.ball;
+
+  if (ball.y + ball.speed * ball.directionY > playgroundHeight)
+  {
+    ball.directionY = -1;
+  }
+  if (ball.y + ball.speed * ball.directionY < 0)
+  {
+    ball.directionY = 1;
+  }
+
+  // check right edge
+  if (ball.x +ball.speed*ball.directionX > playgroundWidth)
+  {
+    // player B lost.
+    pingpong.scoreA++;
+    $("#scoreA").html(pingpong.scoreA);
+
+    // reset the ball;
+    ball.x = 250;
+    ball.y = 100;
+    $("#ball").css({
+      "left": ball.x,
+      "top" : ball.y
+    });
+    ball.directionX = -1;
+  }
+
+
+  // check left edge
+  if (ball.x + ball.speed * ball.directionX < 0)
+  {
+    // player A lost.
+    pingpong.scoreB++;
+    $("#scoreB").html(pingpong.scoreB);
+
+    // reset the ball;
+    ball.x = 150;
+    ball.y = 100;
+    $("#ball").css({
+      "left": ball.x,
+      "top" : ball.y
+    });
+    ball.directionX = 1;
+  }
+
+
+  ball.x += ball.speed * ball.directionX;
+  ball.y += ball.speed * ball.directionY;
+
+  // check left paddle
+  var paddleAX = parseInt($("#paddleA").css("left"))+parseInt($("#paddleA").css("width"));
+  var paddleAYBottom = parseInt($("#paddleA").css("top"))+parseInt($("#paddleA").css("height"));
+  var paddleAYTop = parseInt($("#paddleA").css("top"));
+  if (ball.x + ball.speed*ball.directionX < paddleAX)
+  {
+    if (ball.y + ball.speed*ball.directionY <= paddleAYBottom && ball.y + ball.speed*ball.directionY >= paddleAYTop)
+    {
+      ball.directionX = 1;
+    }
+  }
+
+
+  // check right paddle
+  var paddleBX = parseInt($("#paddleB").css("left"));
+  var paddleBYBottom = parseInt($("#paddleB").css("top"))+parseInt($("#paddleB").css("height"));
+  var paddleBYTop = parseInt($("#paddleB").css("top"));
+  if (ball.x + ball.speed * ball.directionX >= paddleBX)
+  {
+    if (ball.y + ball.speed * ball.directionY <= paddleBYBottom && ball.y + ball.speed * ball.directionY >= paddleBYTop)
+    {
+      ball.directionX = -1;
+    }
+  }
+
+  $("#ball").css({
+    "left" : ball.x,
+    "top" : ball.y
+  });
 }
 
